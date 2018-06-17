060d3f6e48487d157485af9da86dc32165395d80
Sakshi013 <sakshibhat013@gmail.com>
2018-06-17 17:56:46 +0530

    Bracket issue

--- app/src/main/java/com/example/sakshi/intent/MainActivity.java
+++ app/src/main/java/com/example/sakshi/intent/MainActivity.java
@@ -6,32 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button bttn;
-
-    }
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       Button bttn = findViewById(R.id.button);
-                bttn.setOnClickListener(new View.OnClickListener() {
+       
+        Button bttn = findViewById(R.id.button);
+        bttn.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent myActivity =new Intent(MainActivity.this, secondActivity.class)
-                                startActivity(myActivity)
-
-                    }
-                }
-
-
-    }
-
-
-
-
-
-
-    }
+                        Intent myActivity =new Intent(MainActivity.this, secondActivity.class);
+                                startActivity(myActivity);
+                        }
+                    });
+        }
 }
